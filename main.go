56837d2166ccf409a1e092d357483b9aab3e28e1
vzaycev <vzaycev@nic.ru>
2019-07-24 23:29:07 +0300

    0.1.0 go formatted

--- main.go
+++ main.go
@@ -3,14 +3,16 @@ package main
 import (
 	"github.com/Syfaro/telegram-bot-api"
 	"log"
+	"math/rand"
 	"net/http"
 	"net/url"
 	"strings"
-	"math/rand"
 	"time"
 )
-var WorkAnswers = [...]string {"Ты хотел сказать \"чиллить\"?", "Ну я лично скоро на Охотном чилить буду", "Опять на работу пиздос"}
-func RandomizeAnswers(Collection [3]string)string {
+
+var WorkAnswers = [...]string{"Ты хотел сказать \"чиллить\"?", "Ну я лично скоро на Охотном чилить буду", "Опять на работу пиздос"}
+
+func RandomizeAnswers(Collection [3]string) string {
 	s1 := rand.NewSource(time.Now().UnixNano())
 	AnsIndex := rand.New(s1).Intn(len(Collection))
 	return Collection[AnsIndex]
@@ -23,7 +25,7 @@ func main() {
 	transport := &http.Transport{Proxy: http.ProxyURL(proxyUrl)}
 	client := &http.Client{Transport: transport}
 	// Auth section
-	bot, err := tgbotapi.NewBotAPIWithClient("token", client)             //TODO: configs->config.yaml
+	bot, err := tgbotapi.NewBotAPIWithClient("token", client) //TODO: configs->config.yaml
 	if err != nil {
 		log.Panic(err)
 	}
@@ -36,7 +38,7 @@ func main() {
 		if update.Message == nil {
 			continue
 		}
-		if update.Message.IsCommand(){
+		if update.Message.IsCommand() {
 			msg := tgbotapi.NewMessage(update.Message.Chat.ID, "")
 			switch update.Message.Command() {
 			case "about":
@@ -50,27 +52,27 @@ func main() {
 		}
 		log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
 		msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
-			switch {
-				case strings.Contains(msg.Text, "работать"):
-					msg.ReplyToMessageID = update.Message.MessageID
-					msg.Text = RandomizeAnswers(WorkAnswers)
-					bot.Send(msg)
-				case strings.Contains(msg.Text, "финик"):
-					msg.ReplyToMessageID = update.Message.MessageID
-					msg.Text = "Ну вообще-то финик заебал меня хейтить"
-					bot.Send(msg)
-				case strings.Contains(msg.Text, "залупин"):
-					msg.ReplyToMessageID = update.Message.MessageID
-					msg.Text = "Залупин бы сначала научился микротик настраивать"
-					bot.Send(msg)
-				case strings.Contains(msg.Text, "справедливо"):
-					msg.ReplyToMessageID = update.Message.MessageID
-					msg.Text = "имхо нет"
-					bot.Send(msg)
-				case strings.Contains(msg.Text, "хорошо плохо"):
-					msg.ReplyToMessageID = update.Message.MessageID
-					msg.Text = "Ну они с фиником вообще-то меня тупо хейтят постоянно"
-					bot.Send(msg)
-				}
+		switch {
+		case strings.Contains(msg.Text, "работать"):
+			msg.ReplyToMessageID = update.Message.MessageID
+			msg.Text = RandomizeAnswers(WorkAnswers)
+			bot.Send(msg)
+		case strings.Contains(msg.Text, "финик"):
+			msg.ReplyToMessageID = update.Message.MessageID
+			msg.Text = "Ну вообще-то финик заебал меня хейтить"
+			bot.Send(msg)
+		case strings.Contains(msg.Text, "залупин"):
+			msg.ReplyToMessageID = update.Message.MessageID
+			msg.Text = "Залупин бы сначала научился микротик настраивать"
+			bot.Send(msg)
+		case strings.Contains(msg.Text, "справедливо"):
+			msg.ReplyToMessageID = update.Message.MessageID
+			msg.Text = "имхо нет"
+			bot.Send(msg)
+		case strings.Contains(msg.Text, "хорошо плохо"):
+			msg.ReplyToMessageID = update.Message.MessageID
+			msg.Text = "Ну они с фиником вообще-то меня тупо хейтят постоянно"
+			bot.Send(msg)
+		}
 	}
 }
